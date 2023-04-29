aba4fa2da0324cc0e8b9d5a6e3a73057fae76c61
Jhonel Villanueva <vvjhonel@gmail.com>
2023-04-30 03:08:01 +0800

    ref: Change filename from reset.css to global.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './stylesheets/reset.css';
+import './stylesheets/global.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
