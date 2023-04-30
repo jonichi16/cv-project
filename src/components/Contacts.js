14bdfe6c02c23591d44c2e22d669da8084fd39cc
Jhonel Villanueva <vvjhonel@gmail.com>
2023-05-01 01:41:59 +0800

    feat: Add contacts details

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,36 +1,66 @@
 import React, { Component } from 'react';
+import SectionInput from './SectionInput';
+import uniqid from 'uniqid';
 
 class Contacts extends Component {
   constructor() {
     super();
 
     this.state = {
-      contactNumber: '',
-      email: '',
-      address: '',
+      parent: 'div',
+      contents: [
+        {
+          id: uniqid(),
+          tag: 'input',
+          name: 'contact-number',
+          class: 'section-input contact-number',
+          value: '',
+          placeholder: 'Contact Number',
+        },
+        {
+          id: uniqid(),
+          tag: 'input',
+          name: 'email',
+          class: 'section-input email',
+          value: '',
+          placeholder: 'Email',
+        },
+        {
+          id: uniqid(),
+          tag: 'input',
+          name: 'address',
+          class: 'section-input address',
+          value: '',
+          placeholder: 'Address',
+        },
+      ],
     };
   }
 
-  handleContactNumber = (e) => {
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    const { contents } = this.state;
+    const index = contents.findIndex((c) => c.name === name);
+    contents[index].value = value;
     this.setState({
-      contactNumber: e.target.value,
-    });
-  };
-
-  handleEmail = (e) => {
-    this.setState({
-      email: e.target.value,
-    });
-  };
-
-  handleAddress = (e) => {
-    this.setState({
-      address: e.target.value,
+      contents,
     });
   };
 
   render() {
-    return <div>Contacts</div>;
+    const { parent, contents } = this.state;
+
+    return (
+      <section className='contacts'>
+        <h2 className='section-title uppercase'>Contacts</h2>
+        <hr />
+        <SectionInput
+          parent={parent}
+          contents={contents}
+          handleChange={this.handleChange}
+        />
+      </section>
+    );
   }
 }
 
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Details from './Details';
+import Contacts from './Contacts';
 
 const Resume = () => (
   <div className='resume'>
     <div className='left'>
       <Details />
+      <Contacts />
     </div>
   </div>
 );
